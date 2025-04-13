647c82499c55790fabbdd56bf8600571f4544fac
unknown <pg6527@srmist.edu.in>
2025-04-13 23:32:05 +0530

    WebDriverManager Testbase File Coverage

---
+++ src/test/java/utils/TestBase.java
@@ -0,0 +1,47 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+public class TestBase {
+
+    public WebDriver driver;
+
+    public WebDriver WebDriverManager() throws IOException
+    {
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"//src//main//resources//global.properties");
+        Properties prop = new Properties();
+        prop.load(fis);
+
+        String url = prop.getProperty("QAUrl");
+        String browser_props = prop.getProperty("browser");
+        String browser_commandLine = System.getProperty("browser");   //to check the browser value entered in maven cmd line
+
+        String browser = browser_commandLine!=null ? browser_commandLine : browser_props;   //command line browser value is the priority, if it's null - the browser value in properties file will be taken.
+
+        if (driver == null) {
+            if (browser.equalsIgnoreCase("Chrome"))
+            {
+//                System.setProperty(System.getProperty("user.dir"),"//src//test//resources//chromedriver");  //Just a Placeholder, might not be needed in latest Selenium version
+                driver = new ChromeDriver();
+            } else if (browser.equalsIgnoreCase("Firefox"))
+            {
+//                System.setProperty(System.getProperty("user.dir"),"//src//test//resources//geckodriver");   // Placeholder
+                driver = new FirefoxDriver();
+            }
+
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+            driver.manage().window().maximize();
+            driver.get(url);
+        }
+
+        return driver;
+
+    }
+}
